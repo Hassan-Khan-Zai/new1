6796547512d02681d589cb8d065d02849db25572
Hassan-Khan-Zai <hassankhanzai55@gmail.com>
2024-10-15 00:09:32 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function calculateAge() {
+    const birthdate = document.getElementById('birthdate').value;
+    const currentDate = document.getElementById('currentdate').value;
+    const resultElement = document.getElementById('result');
+
+    if (birthdate && currentDate) {
+        const birthDateObj = new Date(birthdate);
+        const currentDateObj = new Date(currentDate);
+
+        let years = currentDateObj.getFullYear() - birthDateObj.getFullYear();
+        let months = currentDateObj.getMonth() - birthDateObj.getMonth();
+        let days = currentDateObj.getDate() - birthDateObj.getDate();
+
+        if (days < 0) {
+            months--;
+            days += new Date(currentDateObj.getFullYear(), currentDateObj.getMonth(), 0).getDate();
+        }
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+
+        resultElement.textContent = `Your current Age is ${years} years ${months} months ${days} days`;
+    } else {
+        resultElement.textContent = "Please enter both dates.";
+    }
+}
